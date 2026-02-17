41c491e9fec122cb1a9a01fdcf3e3c72e788651c
thishakyadarmawardhana <thishakyadarmawardhana@gmail.com>
2026-02-17 18:30:16 +0530

    Scheduled interviews displayed

--- app/(main)/dashboard/_components/interviewCard.jsx
+++ app/(main)/dashboard/_components/interviewCard.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import moment from 'moment'
 import { Button } from "@/components/ui/button";
-import { Copy } from 'lucide-react';
-import { Send } from 'lucide-react';
+import { Copy, Send, ArrowRight } from 'lucide-react';
 import { toast } from 'react-hot-toast';
 
 
-function InterviewCard({interview}) {
+function InterviewCard({interview, viewDetail=false}) {
   const url = process.env.NEXT_PUBLIC_HOST_URL+ "/"+ interview?.interview_id;
 
   const copyLink=()=>{
@@ -26,12 +25,17 @@ function InterviewCard({interview}) {
       </div>
 
       <h2 className='mt-3 font-bold text-lg'>{interview?.jobPosition}</h2>
-      <h2 className='mt-2 text-sm text-gray-500'>{interview?.duration} minutes</h2>
+      <h2 className='mt-2 text-sm text-gray-500 flex justify-between text-gray-500'>{interview?.duration}
+        <span className='text-green-600'>{interview['interview-feedback']?.length} Candidates</span>
+      </h2>
 
-      <div className='flex gap-3 mt-3'>
+      {!viewDetail ? <div className='flex gap-3 mt-3'>
         <Button variant="outline" className='flex-1 cursor-pointer' onClick={copyLink}><Copy className='w-4 h-4'/>Copy Link</Button>
         <Button className='flex-1 cursor-pointer' onClick={onSend}><Send className='w-4 h-4 '/>Send</Button>
       </div>
+      :
+      <Button className="mt-3 w-full" variant='outline' >View Detail <ArrowRight className="w-4 h-4 ml-2"/></Button>
+      }
      
     </div>
   )
