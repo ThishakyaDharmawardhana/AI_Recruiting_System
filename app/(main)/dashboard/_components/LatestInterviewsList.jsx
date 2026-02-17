47238b499231505b201704e286fbf7600fed1da2
thishakyadarmawardhana <thishakyadarmawardhana@gmail.com>
2026-02-17 16:05:16 +0530

    Previously crated interviews displayed

--- app/(main)/dashboard/_components/LatestInterviewsList.jsx
+++ app/(main)/dashboard/_components/LatestInterviewsList.jsx
@@ -2,10 +2,32 @@
 import { Button } from "@/components/ui/button";
 import React, { useState, useEffect } from "react";
 import { Video } from "lucide-react";
+import { useUser } from "@/app/provider";
+import { supabase } from "@/services/supabaseClient";
+import InterviewCard from "./InterviewCard";
 
 
 function LatestInterviewsList() {
     const [interviewList, setInterviewList] = useState([])
+    const {user} = useUser();
+
+    useEffect(()=>{
+      user && GetInterviewList();
+    }, [user])
+
+    const GetInterviewList=async ()=>{
+      let { data: Interviews, error } = await supabase
+          .from('Interviews')
+          .select('*')
+          .eq('userEmail', user?.email)
+          .order('id', { ascending: false })
+          .limit(6);
+
+      console.log(Interviews)
+      setInterviewList(Interviews);
+
+
+    }
 
   return (
     <div className='my-5'>
@@ -16,8 +38,18 @@ function LatestInterviewsList() {
           <Video className = "h-9 w-9 text-primary" />
           <h2>You don't have any interviews created</h2>
           <Button>+ Create New Interview</Button>
-       </div>
-      }
+       </div>}
+
+       {interviewList&&
+          <div className="grid grid-cols-2 lg:grid-cols-3 mt-5 xl:grid-cols-4 gap-5">
+            {interviewList.map((interview, index)=>(
+              <InterviewCard interview={interview} key={index} />
+
+            ))}
+          </div>
+       
+       
+       }
 
     
     </div>
--- app/(main)/dashboard/_components/interviewCard.jsx
+++ app/(main)/dashboard/_components/interviewCard.jsx
@@ -1,13 +1,40 @@
-{/*import React from 'react'
+import React from 'react'
+import moment from 'moment'
+import { Button } from "@/components/ui/button";
+import { Copy } from 'lucide-react';
+import { Send } from 'lucide-react';
+import { toast } from 'react-hot-toast';
 
-function interviewCard({interview}) {
+
+function InterviewCard({interview}) {
+  const url = process.env.NEXT_PUBLIC_HOST_URL+ "/"+ interview?.interview_id;
+
+  const copyLink=()=>{
+      navigator.clipboard.writeText(url);
+      toast('Link Copied')
+    }
+
+    const onSend=()=>{
+      window.location.href = "mailto:accounts@thishakya7376.com?subject=AiCruiter Interview Link & body=Interview Link:"+ url;
+    }
+
   return (
-    <div>
-      
-      Interview cards
+    <div className='p-5 bg-white rounded-lg border'>
+      <div className='flex items-center justify-between'>
+        <div className='h-[20px] w-[20px] bg-primary rounded-full'></div>
+        <h2 className='text-sm text-gray-600'>{moment(interview?.created_at).format("MMM Do YY")}</h2>
+      </div>
+
+      <h2 className='mt-3 font-bold text-lg'>{interview?.jobPosition}</h2>
+      <h2 className='mt-2 text-sm text-gray-500'>{interview?.duration} minutes</h2>
+
+      <div className='flex gap-3 mt-3'>
+        <Button variant="outline" className='flex-1 cursor-pointer' onClick={copyLink}><Copy className='w-4 h-4'/>Copy Link</Button>
+        <Button className='flex-1 cursor-pointer' onClick={onSend}><Send className='w-4 h-4 '/>Send</Button>
+      </div>
      
     </div>
   )
 }
 
-export default interviewCard*/}
+export default InterviewCard
